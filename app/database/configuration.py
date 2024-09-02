629ad23ac4503e9f2134b75ba79bf0375552ea6a
Daniel Bustamante <danielbustamante1606@gmail.com>
2024-09-02 18:33:49 -0500

    nt78 feat se crean variables de entorno para la conexion a BD

---
+++ app/database/configuration.py
@@ -0,0 +1,6 @@
+dataBaseName ="pruebasdb1"
+userName = "root"
+password = ""
+connectionPort = 3306
+server = "localhost"
+dataBaseConnection= f"mysql+mysqlconnector://{userName}:{password}@{server}:{connectionPort}/{dataBaseName}"
